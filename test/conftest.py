5f7928044bb7bebdd37d2f47650c4e8bd7e53741
Andrey Rassokhin <Andrey.Rassokhin@etalongroup.com>
2023-06-09 16:50:43 +0300

    Подправил фикстуру

--- test/conftest.py
+++ test/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="class", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def brows_size():
     browser.config.window_width = 1900
     browser.config.window_height = 800
